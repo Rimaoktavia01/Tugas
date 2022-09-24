ef0ac792ac5d3fa1e343c72bcf4927fda4c400ad
Rimaoktavia01 <114274622+Rimaoktavia01@users.noreply.github.com>
2022-09-23 21:14:07 -0700

    Add files via upload

---
+++ latihan4a.php
@@ -0,0 +1,29 @@
+<?php
+
+$nama = ["Hijau", "Kuning", "Kelabu", "Merah Muda", "Biru"];
+$angka =  [1,2,3,4];
+
+?> 
+
+<!DOCTYPE html>
+<html>
+<head>
+<title>latihan4a </title>
+</head> 
+<body>
+    <p>Balonku ada lima.<br>
+        Rupa-rupa warna-nya<br>
+
+<?php for ($i=0; $i < 4 ; $i++) { 
+   echo "$nama[$i],";
+    } {
+
+    }
+    
+    ?> dan <?php echo "$nama[4]" ?>
+    <br>Meletus balon <?php echo "$nama[0]";?> DOR!! <br>
+    Hatiku sangat kacau.
+
+    <p>
+</body> 
+</html>
